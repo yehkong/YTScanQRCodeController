02f69a893e2fab334e4c75f719a62a8b48f2aef0
andrew <andrew@andrews-macbook-pro.local>
2019-04-18 17:25:01 +0800

    Initial Commit

---
+++ qrScanDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  qrScanDemo
+//
+//  Created by andrew on 2019/4/18.
+//  Copyright © 2019年 yetaiwen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
